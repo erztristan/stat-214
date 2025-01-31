b4385ecbd858c0cf2f6e07920e321d09477f2a69
erztristan <erztristan@berkeley.edu>
2025-01-31 14:18:51 -0800

    ABC

---
+++ lab0/code/lab0.py
@@ -0,0 +1,17 @@
+# 8. To make changes:
+#    a. Stage changes
+# git add .
+#    b. Commit changes
+# git commit -m "Your commit message"
+#    c. Push to your repository
+# git push origin main
+
+# 9. To pull future changes from the original repository
+#git fetch upstream
+#git merge upstream/main
+# Then push to your repo
+#git push origin main
+
+# Alternatively:
+#git pull upstream main
+#git push origin main
